905f4582ecd947449c100b9131c96cc9846b0574
uvsq22104458 <many.afaghpour@ens.uvsq.fr>
2024-02-16 18:45:39 +0100

    Cleaning and correcting everything.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# Constantes
+############
+# Constant #
+############
 LARGEUR = 7
 HAUTEUR = 6
-
-# Variables
+#############
+# Variables #
+#############
 user_toggle = True
-
-# Fonctions
+#############
+# Functions #
+#############
 
 
 def grid_create():
+    # Create a grid full of '.'.
     grid = []
     for i in range(LARGEUR):
         grid.append([])
@@ -18,105 +23,95 @@ def grid_create():
 
 
 def rules(grid, user, user_toggle):
-    # rules for user_1 using 0 as pions
-    if user_toggle == True:
-        for i in grid[user]:
-            # rules for adding a pion
-            if grid[user][-i:] == '.':
-                grid[user][-i:] = 0
+    # Rules for adding a pawn, verify if the column if full or if it's a draw.
+    # Check if there is any horizontally, vertically or diagonally match.
+    # Switch to the next user after the first user played.
+    compteur_0 = 0
+    compteur_1 = 0
+    # Add a pawn.
+    for i in range(1, LARGEUR + 1):
+        if grid[user][-i] == '.':
+            if user_toggle == True:
+                grid[user][-i] = 0
                 break
-            # rules if the colunm is full
-            elif grid[user][0] != '.':
-                print(
-                    'La colonne est complète, veuillez réessayer.')
-                user_toggle = True
-                input_user(grid, user_toggle)
-    # rules for user_2 using 1 as pions
-    if user == False:
-        for i in grid[user]:
-            # rules for adding a pion
-            if grid[user][-i:] == '.':
-                grid[user][-i:] = 1
+            else:
+                grid[user][-i] = 1
                 break
-            # rules if the colunm is full
-            elif grid[user][0] != '.':
-                print(
-                    'La colonne est complète, veuillez réessayer.')
-                user_toggle = False
-                input_user(grid, user_toggle)
-    # rules for a draw
-    compteur = 0
+    # Check if the colunm is full.
+        elif grid[user][0] != '.':
+            print(
+                'La colonne est complète, veuillez réessayer.')
+            input_user(grid, user_toggle)
+    # Check for a draw
     for i in range(LARGEUR):
-        for j in range(1):
-            if grid[i][j] == 0 or grid[i][j] == 1:
-                compteur += 1
-            if compteur == LARGEUR:
-                print('Le puissance 4 est complet, la partie est nulle.')
+        if grid[i][0] == 0 or grid[i][0] == 1:
+            compteur_0 += 1
+        if compteur_0 == LARGEUR:
+            print('Le puissance 4 est complet, la partie est nulle.')
+            compteur_0 = 0
+            break
+    # Check if there is a match vertically.
+    for i in range(LARGEUR):
+        for j in range(HAUTEUR):
+            if grid[i][j] == 0:
+                compteur_0 += 1
+            elif grid[i][j] == 1:
+                compteur_1 += 1
+            elif compteur_1 == 4 or compteur_0 == 4:
+                print('Partie Terminée')
+                compteur_1 = 0
+                compteur_0 = 0
                 break
-
-    # rules to check if there is a match vertically
-    if user_toggle == True:
-        for i in range(len(grid)):
-            if grid[i].count(0) == 4:
-                print('Partie Gagnée par user_1')
+            else:
+                compteur_1 = 0
+                compteur_0 = 0
+    # Check if there is a match horizontally.
+    for i in range(HAUTEUR):
+        for j in range(LARGEUR):
+            if grid[j][i] == 0:
+                compteur_0 += 1
+            elif grid[j][i] == 1:
+                compteur_1 += 1
+            elif compteur_1 == 4 or compteur_0 == 4:
+                print('Partie Terminée')
+                compteur_1 = 0
+                compteur_0 = 0
+                break
+            else:
+                compteur_1 = 0
+                compteur_0 = 0
+    # Check if there is a match diagonally.
+    for i in range(LARGEUR):
+        for j in range(HAUTEUR):
+            if grid[i:i+3][j:j+3] == [0, 0, 0, 0] or grid[i:i+3][j:j+3] == [1, 1, 1, 1] or grid[i:i+3][j:j-3] == [0, 0, 0, 0] or grid[i:i+3][j:j-3] == [1, 1, 1, 1]:
+                print('Partie Terminée')
+                break
+    # Switching to the other user if the first user played.
     if user_toggle == False:
-        for i in range(len(grid)):
-            if grid[i].count(0) == 4:
-                print('Partie Gagnée par user_2')
-
-    # rules to check if there is a match horizontally
+        user_toggle = True
+        input_user(grid, user_toggle)
     if user_toggle == True:
-        compteur = 0
-        for i in range(len(grid)):
-            for j in range(LARGEUR):
-                if grid[j][i] == 0:
-                    compteur += 1
-                elif compteur == 4:
-                    print('Partie Gagnée par user_1')
-                    break
-                else:
-                    compteur = 0
-    if user_toggle == False:
-        compteur = 0
-        for i in range(len(grid)):
-            for j in range(LARGEUR):
-                if grid[j][i] == 0:
-                    compteur += 1
-                elif compteur == 4:
-                    print('Partie Gagnée par user_2')
-                    break
-                else:
-                    compteur = 0
-
-    # rules to check if there is a match diagonally
-    if user_toggle == True:
-        compteur = 0
-        for i in range(len(grid)):
-            for j in range(LARGEUR):
-                if grid[grid[i][j]:grid[i+3][j+3]] == [0, 0, 0, 0] or grid[grid[i][j]:grid[i+3][j-3]] == [0, 0, 0, 0]:
-                    print('Partie Gagnée par user_1')
-                    break
-    if user_toggle == False:
-        compteur = 0
-        for i in range(len(grid)):
-            for j in range(LARGEUR):
-                if grid[grid[i][j]:grid[i+3][j+3]] == [0, 0, 0, 0] or grid[grid[i][j]:grid[i+3][j-3]] == [0, 0, 0, 0]:
-                    print('Partie Gagnée par user_1')
-                    break
+        user_toggle = False
+        input_user(grid, user_toggle)
 
 
 def input_user(grid, user_toggle):
+    # Check if the turn is for user_0 or user_1.
+    # user_0 start first.
     if user_toggle == True:
-        user_1 = int(
-            input('Entrez le n° de la colonne souhaitée pour jouer : ')) - 1
-        rules(grid, user_1, user_toggle)
+        display_grid(grid)
+        user_0 = int(
+            input('User_0 le n° de la colonne souhaitée pour jouer : ')) - 1
+        rules(grid, user_0, user_toggle)
     if user_toggle == False:
-        user_2 = int(
-            input('Entrez le n° de la colonne souhaitée pour jouer : ')) - 1
-        rules(grid, user_2, user_toggle)
+        display_grid(grid)
+        user_1 = int(
+            input('User_1 le n° de la colonne souhaitée pour jouer : ')) - 1
+        rules(grid, user_1, user_toggle)
 
 
 def display_grid(grid):
+    # Display each list of the grid vertically to form a real connect-four
     for i in range(HAUTEUR):
         for j in range(LARGEUR):
             if j != LARGEUR - 1:
@@ -125,12 +120,12 @@ def display_grid(grid):
                 print(grid[j][i], end='\n')
 
 
-# Main
-
-
+########
+# Main #
+########
 def main():
+    # Main function where the interpreter start it first.
     grid = grid_create()
-    display_grid(grid)
     input_user(grid, user_toggle)
 
 
